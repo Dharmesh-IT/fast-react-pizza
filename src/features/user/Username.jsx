2c081722542707571afc076c511c651258e4e828
Dharmesh Sharma <info@dharmeshsharma.com>
2025-05-31 01:34:41 +0530

    feat: integrate Redux for user management, add username handling, and update UI components

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-export default function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Dharmesh</div>;
+function Username() {
+  const username = useSelector((state) => state.user.username);
+  if (!username) {
+    return null; // Return null if username is not set
+  }
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
+
+export default Username;
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,19 +1,27 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
   return (
-    <div className="my-10 sm:my-16  px-4 text-center">
-      <h1 className="text-xl font-bold text-center mb-8 md:text-3xl" >
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-center text-xl font-bold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
-
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Button type="primary" to="/menu">
+          continue ordering {username}
+        </Button>
+      )}
     </div>
   );
 }
-;;
 export default Home;
